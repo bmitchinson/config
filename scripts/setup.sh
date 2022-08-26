0cf91c308ec2d3e2ca931022c906e828115c85f0
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-08-25 23:03:38 -0600

    Only install brew on MacOS

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,6 @@ echo "Did you install ohmyzsh first? (check readme)"
 
 echo "👷‍♂️ - Running setup"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -12,6 +10,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 export config_repo="${HOME}/repos/config"
 
 if test "$(uname)" = "Darwin" ; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   cp -r $config_repo/FiraFont/. /Library/Fonts
 fi
 
